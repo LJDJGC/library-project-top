4c291a05b298a6831b19b62cf46257303acd26ed
LJDJGC <singsongsmusic@gmail.com>
2026-02-02 05:50:02 +0900

    feat: implement Book constructor and storage logic

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+  // take params, create a book then store it in the array
+  const newbook = new Book(title, author, pages, isRead);
+
+  myLibrary.push(newbook);
+}
+
+addBookToLibrary("The Hobbit", "Tolkien", 295, false);
+addBookToLibrary("1984", "George Orwell", 328, true);
+console.log(myLibrary);
